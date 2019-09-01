77decf5eae19ba7b49fdef6fc06e2612a5586dcd
Sean Davis <sean.dev.davis@gmail.com>
2019-09-01 15:53:21 -0500

    wip

---
+++ Corporate Class/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Corporate_Class
+{
+    public class Employee
+    {
+        public string name;
+        public string jobTitle;
+        public DateTime dateTime;
+
+        public Employee(string name, string jobTitle, DateTime dateTime)
+        {
+            this.name = name;
+            this.jobTitle = jobTitle;
+            this.dateTime = dateTime;
+        }
+    }
+}
--- Corporate Class/Program.cs
+++ Corporate Class/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Corporate_Class
 {
@@ -11,12 +12,21 @@ namespace Corporate_Class
         public DateTime CreatedOn { get; }
 
         // Create a property for holding a list of current employees
-        public string employees;
+
+        public List<Employee> employees { get; set; } = new List<Employee>();
 
         // Create a method that allows external code to add an employee
-
+        public void addEmployee(string name)
+        {
+            var newEmployee = new Employee(name, "Janitor", DateTime.Today);
+            employees.Add(newEmployee);
+        }
         // Create a method that allows external code to remove an employee
-
+        public void subEmployee(string name)
+        {
+            var term = new Employee(name, "Clerk", DateTime.Today);
+            employees.Remove(term);
+        }
         /*
             Create a constructor method that accepts two arguments:
                 1. The name of the company
@@ -24,7 +34,7 @@ namespace Corporate_Class
     
             The constructor will set the value of the public properties
         */
-        public Company()
+        public Company(string name)
         {
 
         }
@@ -33,7 +43,16 @@ namespace Corporate_Class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter employee name...");
+            var input = Console.ReadLine();
+            var company = new Company("cool company");
+
+            company.addEmployee(input);
+            for (var i = 0; i < company.employees.Count; i++)
+            {
+                Console.WriteLine(company.employees[i].name);
+            }
         }
+
     }
 }
